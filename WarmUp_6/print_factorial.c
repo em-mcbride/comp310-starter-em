bd5a94a47837655afcb9621fb27beb63d5d5adef
em mcbride <emma.c.mcbride@gmail.com>
2024-01-28 22:46:12 +0000

    added WarmUp_6

---
+++ WarmUp_6/print_factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int n) {
+    if (n <= 0) {
+        return 1;
+    } else {
+        return n*factorial(n-1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    //calculate factorial of a number using recursion
+    int number = 5;
+    printf("Factorial of %d is: %d\n", number, factorial(number));
+}
+
