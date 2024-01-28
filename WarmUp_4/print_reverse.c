91e859580d1e8650fdf7e83b5fd6e22e6e3a213c
em mcbride <emma.c.mcbride@gmail.com>
2024-01-28 17:32:46 +0000

    added WarmUp_4

---
+++ WarmUp_4/print_reverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char str[100] = "Hello";
+    int len = 0;
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    for (int i = 0, j = len - 1; i < j; i++, j--) {
+        char c = str[i];
+        str[i] = str[j];
+        str[j] = c;
+    }
+    printf("%s\n", str);
+    return EXIT_SUCCESS;
+}
