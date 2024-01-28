2959048105f3cb61e468ad3cd13047e1b1b0d922
em mcbride <emma.c.mcbride@gmail.com>
2024-01-28 23:56:52 +0000

    added WarmUp_9

---
+++ WarmUp_9/print_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	FILE* file; 
+	char filename[] = "example.txt";
+	
+	file = fopen(filename, "w"); 
+	if(file == NULL) { 
+		perror("Error opening the file"); 
+		return EXIT_FAILURE;
+	}
+
+	fprintf(file, "Hello, World!"); 
+	fclose(file);
+	
+	file = fopen(filename, "r"); 
+	if(file == NULL) { 
+		perror("Error opening the file"); 
+		return EXIT_FAILURE;
+	}
+
+	char line[100]; 
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+		printf("\n");
+    }
+
+	fclose(file);
+
+	return EXIT_SUCCESS;
+}
